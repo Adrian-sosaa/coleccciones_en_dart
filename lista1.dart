5fc3466bbee8b0b5a550f388a5e452d0a79f1c6e
Adrian-sosaa <a.22308051281317@cbtis128.edu.mx>
2025-02-11 16:53:58 +0000

    hola lista

---
+++ lista1.dart
@@ -0,0 +1,14 @@
+void main() {  
+   List<int> numeros = [10,20,30];  
+   print("lista d numeros enteros: ");
+   print(numeros);  
+   print("Primer elemento: ${numeros[0]}");
+   print("Segundo elemento: ${numeros[1]}");
+   print("Tercer elemento: ${numeros[2]}");
+   print("elemento de la lista usando ciclo for: ");
+    for(int i=0; i<numeros.length; i++){
+      print(numeros[i]);
+    }
+
+
+}  
